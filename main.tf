b5ff00dd5d55a2f0e862366225778d169e13d113
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 09:44:01 +0530

    asdf

--- main.tf
+++ main.tf
@@ -8,10 +8,7 @@ module "mysql" {
   ENV                 = var.ENV 
 }
 
-module "redis" {
-  source              = "./vendor/modules/redis"  
-  ENV                 = var.ENV 
-}
+
 
 # We cannot parameterise anything on source of the module. So that means we cannot use the variables.
 # How can I tell to my source module to fetch the code from a branch of my choice ? By default, it only refers to main branch
