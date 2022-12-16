770ca96cd92fb035f5dc893da27a9908a6d51066
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 07:40:43 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -3,19 +3,8 @@ module "docdb" {
   ENV                 = var.ENV 
 }
 
-module "redis" {
-  source              = "./vendor/modules/redis"  
-  ENV                 = var.ENV 
-}
 
-module "mysql" {
-  source              = "./vendor/modules/mysql"  
-  ENV                 = var.ENV 
-}
 
-# module "vpc" {
-#   source = "git::https://github.com/b51-clouddevops/tf-module-vpc.git"
-# }
 
 # We cannot parameterise anything on source of the module. So that means we cannot use the variables.
 # How can I tell to my source module to fetch the code from a branch of my choice ? By default, it only refers to main branch
