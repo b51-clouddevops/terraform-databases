88ac01778d97a5f20a41f4086cb9d308ae9b2b27
mvpc-11232 <mvp2612@hotmail.com>
2022-12-20 08:32:58 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ module "redis" {
   ENV                 = var.ENV 
 }
 
+output "redis" {
+    value = mdoule.redis.redis
+}
+
 # We cannot parameterise anything on source of the module. So that means we cannot use the variables.
 # How can I tell to my source module to fetch the code from a branch of my choice ? By default, it only refers to main branch
 # Alternatives to make it flexible ?
