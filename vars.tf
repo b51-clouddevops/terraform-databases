9d5cb3b4b36685796363f3833f20a4cc9a5f471e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-20 08:57:18 +0530

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -7,8 +7,15 @@
 # variable "DEFAULT_VPC_RT" {}
 
 variable "ENV" {}
+
+# RDS VARIABLES
 variable "RDS_MYSQL_PORT" {}
 variable "RDS_MYSQL_STORAGE" {}
 variable "RDS_MYSQL_ENGINE_VERSION" {}
 variable "RDS_MYSQL_INSTANCE_CLASS" {}
+
+# DOCDB VARIABLES 
+variable "DOCDB_PORT" {}   
+variable "DOCDB_INSTANCE_COUNT" {} 
+variable "DOCDB_PORT_INSTANCE_CLASS" {} 
 
